0792aa62b2e7c1b3d8e99eaac306df519594b1b0
gregpaton08 <gregpaton08@gmail.com>
2014-05-21 23:04:07 -0400

    [mod] Can now turn light on/off. [mod] Improved how alarm is set.

--- LightAlarm/src/main/java/com/example/lightalarm/AlarmBroadcastReceiver.java
+++ LightAlarm/src/main/java/com/example/lightalarm/AlarmBroadcastReceiver.java
@@ -3,18 +3,122 @@ package com.example.lightalarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by Greg on 5/20/14.
  */
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
+    public static final String TURN_WIFI_OFF = "turn_wifi_off";
+
+    Context m_context = null;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("LightAlarn", "Broadcast Received");
-        Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show();
+
+        m_context = context;
+
+        SharedPreferences preferences = context.getSharedPreferences(MainActivity.PREFS_NAME, 0);
+        String ip = preferences.getString(MainActivity.LIGHT_IP_KEY, MainActivity.PREFS_ERROR);
+        if (ip.equals(MainActivity.PREFS_ERROR)) {
+            return;
+        }
+
+        String url = "http://" + ip + "/light_on";
+
+        String wifiStatus = "";
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (false == wifiManager.isWifiEnabled()) {
+            wifiStatus = TURN_WIFI_OFF;
+            wifiManager.setWifiEnabled(true);
+        }
+
+        new HTTPRequest(context).execute(url, wifiStatus);
+    }
+
+    public class HTTPRequest extends AsyncTask<String, String, String> {
+
+        private Context m_context;
+
+        public HTTPRequest(Context context) {
+            m_context = context;
+        }
+
+        private boolean isWifiConnected(Context context) {
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                if (ConnectivityManager.TYPE_WIFI == networkInfo.getType()) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        @Override
+        protected String doInBackground(String... url) {
+            HttpClient httpClient= new DefaultHttpClient();
+            HttpResponse response;
+            String ret = null;
+            try {
+                int timeout = 0;
+                while (false == isWifiConnected(m_context) &&
+                       timeout++ < 15) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+
+                    }
+                }
+
+                URI uri = new URI(url[0]);
+                response = httpClient.execute(new HttpGet(uri));
+            } catch (ClientProtocolException e) {
+                ret = "ClientProtocolException";
+            } catch (IOException e) {
+                ret = "IOException";
+            } catch (URISyntaxException e) {
+                ret = "URISyntaxException";
+            }
+
+            if (url.length > 1) {
+                if (url[1].equals(TURN_WIFI_OFF)) {
+                    WifiManager wifiManager = (WifiManager) m_context.getSystemService(Context.WIFI_SERVICE);
+                    wifiManager.setWifiEnabled(false);
+                }
+            }
+
+            return ret;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if (null != result) {
+                Toast.makeText(m_context, result, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
--- LightAlarm/src/main/java/com/example/lightalarm/MainActivity.java
+++ LightAlarm/src/main/java/com/example/lightalarm/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.lightalarm;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,18 +27,37 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import org.apache.http.client.methods.HttpGet;
+
+import java.text.DateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
+    public static String PREFS_NAME = "main_activity_prefs";
+    public static String PREFS_ERROR = "prefs_error";
+    public static String LIGHT_IP_KEY = "light_url_key";
+
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
-    private CheckBox checkbox_alarm;
-    private TimePicker timePicker_alarm;
+
+    int m_minute;
+    int m_hour;
+
+    // UI Components
+    Button button_lightOn;
+    Button button_lightOff;
+    Button button_tvAlarmTime;
+    TextView textview_alarmTime;
+    ToggleButton toggle_alarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,34 +80,167 @@ public class MainActivity extends Activity {
     protected void onStart() {
         super.onStart();
 
-        checkbox_alarm = (CheckBox)findViewById(R.id.checkbox_alarm);
+        button_lightOn = (Button)findViewById(R.id.button_lightOn);
+        button_lightOff = (Button)findViewById(R.id.button_lightOff);
+        textview_alarmTime = (TextView)findViewById(R.id.textview_alarmTime);
+        button_tvAlarmTime = (Button)findViewById(R.id.button_tvAlarmTime);
+        toggle_alarm = (ToggleButton)findViewById(R.id.toggle_alarm);
 
-        timePicker_alarm = (TimePicker)findViewById(R.id.timepicker_alarmtime);
-        timePicker_alarm.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+        int btWidth = (getWindowManager().getDefaultDisplay().getWidth() / 2) - 40;
+        button_lightOn.setWidth(btWidth);
+        button_lightOff.setWidth(btWidth);
+
+        button_tvAlarmTime.setBackgroundColor(Color.TRANSPARENT);
+    }
+
+    public void onClickButtonLightOn(View view) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+        String ip = preferences.getString(LIGHT_IP_KEY, PREFS_ERROR);
+        if (ip.equals(PREFS_ERROR)) {
+            AlertDialog.Builder urlAlert = new AlertDialog.Builder(this);
+            urlAlert.setTitle("IP Address");
+            urlAlert.setMessage("Set ip address and port (ip:port)");
+            final EditText input = new EditText(this);
+            urlAlert.setView(input);
+            urlAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String inputText = input.getText().toString();
+
+                    SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString(LIGHT_IP_KEY, inputText);
+                    editor.commit();
+
+                    setLight(true);
+                }
+            });
+            urlAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            urlAlert.show();
+        }
+        else {
+            setLight(true);
+        }
+    }
+
+    public void onClickButtonLightOff(View view) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+        String ip = preferences.getString(LIGHT_IP_KEY, PREFS_ERROR);
+        if (ip.equals(PREFS_ERROR)) {
+            AlertDialog.Builder urlAlert = new AlertDialog.Builder(this);
+            urlAlert.setTitle("IP Address");
+            urlAlert.setMessage("Set ip address and port (ip:port)");
+            final EditText input = new EditText(this);
+            urlAlert.setView(input);
+            urlAlert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String inputText = input.getText().toString();
+
+                    SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString(LIGHT_IP_KEY, inputText);
+                    editor.commit();
+
+                    setLight(false);
+                }
+            });
+            urlAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            urlAlert.show();
+        }
+        else {
+            setLight(false);
+        }
+    }
+
+    public void setLight(boolean on) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        String ip = preferences.getString(LIGHT_IP_KEY, PREFS_ERROR);
+        if (ip.equals(PREFS_ERROR)) {
+            return;
+        }
+
+        String url = "http://" + ip;
+        if (on) {
+            url += "/light_on";
+        }
+        else {
+            url += "/light_off";
+        }
+
+        // Check if WIFI is on. If off then turn on
+        String wifiStatus = "";
+        WifiManager wifiManager = (WifiManager) getBaseContext().getSystemService(Context.WIFI_SERVICE);
+        if (false == wifiManager.isWifiEnabled()) {
+            wifiStatus = AlarmBroadcastReceiver.TURN_WIFI_OFF;
+            wifiManager.setWifiEnabled(true);
+        }
+
+        AlarmBroadcastReceiver abr = new AlarmBroadcastReceiver();
+        abr.new HTTPRequest(getBaseContext()).execute(url/*, wifiStatus*/);
+    }
+
+    public void onClickTextViewAlarmTime(View view) {
+
+        String time = textview_alarmTime.getText().toString();
+        if (time.length() == 0) {
+            return;
+        }
+
+        int hour = Integer.parseInt(time.substring(0, time.indexOf(":")));
+        int minute = Integer.parseInt(time.substring(time.indexOf(":") + 1, time.indexOf(" ")));
+        if (-1 != time.indexOf("PM")) {
+            hour += 12;
+        }
+
+        setAlarmDialog(MainActivity.this, hour, minute);
+    }
+
+    public void onToggleAlarm(View view) {
+        ToggleButton tb = (ToggleButton)view;
+
+        if (tb.isChecked()) {
+            setAlarmDialog(MainActivity.this);
+        }
+        else {
+            cancelAlarm(getBaseContext());
+        }
+    }
+
+    public void setAlarmDialog(Context context) {
+        setAlarmDialog(context, -1, -1);
+    }
+
+    public void setAlarmDialog(Context context, int hour, int minute) {
+
+        if (hour < 0 || hour > 24 || minute < 0 || minute > 59) {
+            Calendar calendar = Calendar.getInstance();
+            hour = calendar.get(Calendar.HOUR_OF_DAY);
+            minute = calendar.get(Calendar.MINUTE);
+        }
+
+        TimePickerDialog tpd = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
                 Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.HOUR_OF_DAY, hour);
                 calendar.set(Calendar.MINUTE, minute);
 
                 setAlarm(calendar);
             }
-        });
-    }
-
-    public void onCheckboxAlarmClicked(View view) {
-        boolean checked = ((CheckBox) view).isChecked();
-        CheckBox cb = (CheckBox) view;
-
-        if (checked) {
-            cb.setText("On");
-            setAlarmForCurrentTime();;
-        }
-        else {
-            cb.setText("Off");
-            cancelAlarm(getBaseContext());
-        }
+        }, hour, minute, false);
+        tpd.show();
     }
 
     private PendingIntent createAlarmIntent(Context context) {
@@ -87,36 +248,42 @@ public class MainActivity extends Activity {
         return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 
-    private void setAlarm(Calendar calendar) {
+    private Calendar setAlarm(Calendar calendar) {
         Context context = getBaseContext();
 
         // Cancel any current alarms
         cancelAlarm(context);
 
-        if (checkbox_alarm.isChecked()) {
-            alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-            alarmIntent = createAlarmIntent(context);
+        alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = createAlarmIntent(context);
 
-            alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP,
-                    calendar.getTimeInMillis(),
-                    1000 * 60 * 60 * 24,
-                    alarmIntent);
-            String text = "Alarm set for " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
-            Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP,
+                calendar.getTimeInMillis(),
+                1000 * 60 * 60 * 24,
+                alarmIntent);
+
+//        Toast.makeText(context,
+//                       "Alarm set for " + calendar.get(Calendar.HOUR_OF_DAY) +
+//                       ":" + calendar.get(Calendar.MINUTE),
+//                       Toast.LENGTH_SHORT).show();
+
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        String time = "";
+        time += hour % 12 + ":";
+        if (minute < 10) {
+            time += "0";
+        }
+        time += minute;
+        if (hour > 12) {
+            time += " PM";
         }
         else {
-            String text = "Alarm NOT set for " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE);
-            Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            time += " AM";
         }
-    }
+        textview_alarmTime.setText(time);
 
-    private void setAlarmForCurrentTime() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, timePicker_alarm.getCurrentHour());
-        calendar.set(Calendar.MINUTE, timePicker_alarm.getCurrentMinute());
-
-        setAlarm(calendar);
+        return calendar;
     }
 
     private void cancelAlarm(Context context) {
@@ -124,6 +291,8 @@ public class MainActivity extends Activity {
         alarmIntent = createAlarmIntent(context);
 
         alarmMgr.cancel(alarmIntent);
+
+        textview_alarmTime.setText("");
     }
 
     /**
@@ -141,5 +310,4 @@ public class MainActivity extends Activity {
             return rootView;
         }
     }
-
 }
